91371f54cac25d95a1351e67a455a40a5a13f1e6
Yash <salatyash@gmail.com>
2026-08-16 11:06:51 +0530

    audio inserted

---
+++ enginee/features.py
@@ -0,0 +1,6 @@
+from playsound import playsound
+
+def play_audio():
+    music_path="www\\assests\\audio\\start_sound.mp3"
+    playsound(music_path)
+    
--- www/main.js
+++ www/main.js
@@ -10,4 +10,24 @@ $(document).ready(function () {
       effect: "bounceOut",
     },
   });
+  var siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 900,
+    height: 200,
+    autoStart: true,
+  });
+
+  $(".siri-message").textillate({
+    loop: true,
+    sync: true,
+    in: {
+      effect: "fadeInUp",
+      sync: true,
+    },
+
+    out: {
+      effect: "fadeOutDown",
+      sync: true,
+    },
+  });
 });
